127d3c8319888ca0f1059a2d08725d724e679850
ZLKapps <zlkapps@gmail.com>
2016-02-20 23:14:46 -0500

    added base of the nationality game

---
+++ app/src/main/java/com/zlkapps/guessthenationality/TitleActivity.java
@@ -0,0 +1,15 @@
+package com.zlkapps.guessthenationality;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import zlkapps.guessthenationality.R;
+
+public class TitleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_title);
+    }
+}
